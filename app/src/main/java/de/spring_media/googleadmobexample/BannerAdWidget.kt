cbe5ac7891e6bdabf666a9211c3139a5c68b38e1
skrawczy <sebastian.krawczyk@spring-media.de>
2020-03-19 14:45:58 +0100

    use WidgetAdapter

---
+++ app/src/main/java/de/spring_media/googleadmobexample/BannerAdWidget.kt
@@ -0,0 +1,31 @@
+package de.spring_media.googleadmobexample
+
+import android.view.View
+import com.google.android.gms.ads.AdListener
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import de.welt.widgetadapter.Widget
+import kotlinx.android.synthetic.main.banner_ad_viewholder.view.*
+
+
+class BannerAdWidget: Widget<AdRequest>(R.layout.banner_ad_viewholder) {
+
+    lateinit var bannerAd: AdView
+
+    override fun onViewCreated(view: View) {
+        bannerAd = view.adView
+    }
+
+    override fun setData(data: AdRequest) {
+        bannerAd.adListener = object : AdListener() {
+            override fun onAdLoaded() {}
+            override fun onAdFailedToLoad(p0: Int) {}
+            override fun onAdOpened() {}
+            override fun onAdClicked() {}
+            override fun onAdLeftApplication() {}
+            override fun onAdClosed() {}
+        }
+
+        bannerAd.loadAd(data)
+    }
+}
--- app/src/main/java/de/spring_media/googleadmobexample/MainActivity.kt
+++ app/src/main/java/de/spring_media/googleadmobexample/MainActivity.kt
@@ -1,12 +1,17 @@
 package de.spring_media.googleadmobexample
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.MobileAds
+import de.welt.widgetadapter.WidgetAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val numOfBanners = 25
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,10 +20,20 @@ class MainActivity : AppCompatActivity() {
         MobileAds.initialize(this) {}
 
         val linearLayoutManager = LinearLayoutManager(this)
-        val recyclerViewAdapter = RecyclerViewAdapter()
+        val recyclerViewAdapter = WidgetAdapter(LayoutInflater.from(this@MainActivity)).apply {
+            (0 until numOfBanners).forEach { _ ->
+                addWidget { BannerAdWidget() }
+            }
+        }
         recyclerView.apply {
             layoutManager = linearLayoutManager
             adapter = recyclerViewAdapter
+            val items = mutableListOf<AdRequest>()
+            (0 until numOfBanners).forEach { _ ->
+                items.add(AdRequest.Builder().build())
+            }
+
+            recyclerViewAdapter.setItems(items)
         }
     }
 }
